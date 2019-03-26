604de763db4fddd52ad02322d6aa79ce6a0214d2
Bas Mensinga <81314@b5-450-06.ict.lab.locals>
2019-03-26 14:39:47 +0100

    css rel added

--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -8,7 +8,7 @@
 
       @yield('javascript')
         <link href="https://fonts.googleapis.com/css?family=Montserrat:500,700" rel="stylesheet">
-        <link href="{{asset('css/style.css')}}">
+        <link href="{{asset('css/style.css')}}" rel="stylesheet">
         <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
       @yield('css')
 
