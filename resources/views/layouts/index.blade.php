fbf1257c4364652cffb8a2433cc8c32b38f4760e
Bas Mensinga <81314@b5-450-06.ict.lab.locals>
2019-03-26 16:48:09 +0100

    compute responsive main page

--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -6,10 +6,13 @@
 
         <title>Home</title>
 
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
       @yield('javascript')
         <link href="https://fonts.googleapis.com/css?family=Montserrat:500,700" rel="stylesheet">
         <link href="{{asset('css/style.css')}}" rel="stylesheet">
         <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/themes/smoothness/jquery-ui.css">
       @yield('css')
 
   </head>
