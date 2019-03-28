49f4dfad2f981825a7dd22592e5dc25c526b6002
Bas Mensinga <81314@b5-400b-20.ict.lab.locals>
2019-03-28 09:15:58 +0100

    pushing just for Joey :)

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -5,22 +5,35 @@
 @endsection
 
 @section('css')
-
+<link href="{{asset('css/card.css')}}" rel="stylesheet">
 @endsection
 
 
 @section('content')
 
+<div class="row">
+  <div class="col card">
     {!! Form::model($user, ['method'=>'PATCH', 'action'=>['ProfileController@update', $user->id], 'class' => 'postForm']) !!}
+    <div class="card">
+      <div class="card-header">
+        Instellingen
+      </div>
+      <div class="card-body">
+        <blockquote class="blockquote mb-0">
+          naam: <input type="text" name="name" value="{{$user->name}}" required><br>
+          email: <input type="email" name="email" value="{{$user->email}}" required><br>
+          nieuw wachtwoord: <input type="password" name="newPassword"><br>
+          herhaal wachtwoord: <input type="password" name="newPassword2"><br>
 
-        naam: <input type="text" name="name" value="{{$user->name}}" required><br>
-        email: <input type="email" name="email" value="{{$user->email}}" required><br>
-        nieuw wachtwoord: <input type="password" name="newPassword"><br>
-        herhaal wachtwoord: <input type="password" name="newPassword2"><br>
+          * jouw wachtwoord: <input type="password" name="password" required><br>
 
-        * jouw wachtwoord: <input type="password" name="password" required><br>
+          <button type="submit" class="btn btn-primary">Verander</button>
+        </blockquote>
+      </div>
+    </div>
+  </div>
+</div>
 
-        <input type="submit">
 
     {!! Form::close() !!}
 
