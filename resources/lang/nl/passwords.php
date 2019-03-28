259362035b372b007dd1dd159514e145e0ad1dee
gamefreak20 <joeystil3@gmail.com>
2019-03-28 10:41:18 +0100

    nl language

---
+++ resources/lang/nl/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'password' => 'Je wachtwoord moet minimaal 8 characters zijn en de onfirmation moet kloppen.',
+    'reset' => 'Je wachtwoord is gereset!',
+    'sent' => 'We hebben je een email verzonden met de link!',
+    'token' => 'De wachtwoord reset token klopt niet.',
+    'user' => "We kunnen geen gebruiker vinden met deze e-mail.",
+
+];
