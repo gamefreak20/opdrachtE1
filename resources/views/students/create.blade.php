3e28bd26227735b24e41e654963a9218a366ea62
Bas Mensinga <81314@b5-400b-20.ict.lab.locals>
2019-04-04 09:35:09 +0200

    create student done

--- resources/views/students/create.blade.php
+++ resources/views/students/create.blade.php
@@ -5,20 +5,39 @@
 @endsection
 
 @section('css')
-
+<link href="{{asset('css/card.css')}}" rel="stylesheet">
 @endsection
 
 
 @section('content')
 
+<div class="row">
+  <div class="col card">
     {!! Form::open(['method'=>'POST', 'action'=>['StudentsController@store'], 'class' => '']) !!}
-
-        Studenten nummer: <input type="number" name="student_number">
-        Naam: <input type="text" name="name">
-        Tussenvoegsel: <input type="text" name="insertion">
-        Achternaam: <input type="text" name="last_name">
-        <button type="submit">Maak aan</button>
-
+    <div class="card">
+      <div class="card-header">
+        Maak student aan
+      </div>
+      <div class="card-body">
+        <blockquote class="blockquote mb-0">
+          <div class="form-row">
+            <div class="form-group col-md-4">
+              <input class="form-control" type="text" name="name" required placeholder="Voornaam">
+            </div>
+            <div class="form-group col-md">
+              <input class="form-control" type="text" name="insertion" placeholder="Tussenvoegsel">
+            </div>
+            <div class="form-group col-md-4">
+              <input class="form-control" type="text" name="last_name" required placeholder="Achternaam">
+            </div>
+          </div>
+          <input class="form-control" type="number" name="student_number" required placeholder="Studentennummer">
+          <button type="submit" class="btn btn-primary"><p>Maak aan</p></button>
+        </blockquote>
+      </div>
+    </div>
     {!! Form::close() !!}
+  </div>
+</div>
 
 @endsection
