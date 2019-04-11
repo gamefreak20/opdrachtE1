4d0714440054f03c0be846938968f389f37b6820
Bas Mensinga <81314@b5-400b-14.ict.lab.locals>
2019-04-11 09:12:15 +0200

    create klas done

--- resources/views/classes/create.blade.php
+++ resources/views/classes/create.blade.php
@@ -5,17 +5,32 @@
 @endsection
 
 @section('css')
-
+<link href="{{asset('css/card.css')}}" rel="stylesheet">
 @endsection
 
 
 @section('content')
 
+<div class="row">
+  <div class="col card">
     {!! Form::open(['method'=>'POST', 'action'=>['ClassesController@store'], 'class' => '']) !!}
-
-        Naam: <input type="text" name="name">
-        <button type="submit">Maak aan</button>
-
+    <div class="card">
+      <div class="card-header">
+        Maak klas aan
+      </div>
+      <div class="card-body">
+        <blockquote class="blockquote mb-0">
+          <div class="form-row">
+            <div class="form-group col">
+              <input class="form-control" type="text" name="name" placeholder="Klas naam...">
+            </div>
+          </div>
+          <button type="submit" class="btn btn-primary"><p>Maak aan</p></button>
+        </blockquote>
+      </div>
+    </div>
     {!! Form::close() !!}
+  </div>
+</div>
 
 @endsection
