fe82834af50ab33d8a90f83f6ca92f473f683fe6
Bas Mensinga <81314@b5-400b-19.ict.lab.locals>
2019-04-18 10:48:23 +0200

    table done

--- resources/views/students/show.blade.php
+++ resources/views/students/show.blade.php
@@ -6,7 +6,8 @@
 @endsection
 
 @section('css')
-
+  <link href="{{asset('css/card.css')}}" rel="stylesheet">
+  <link href="{{asset('css/studentDetail.css')}}" rel="stylesheet">
 @endsection
 
 
@@ -14,49 +15,59 @@
 
     @php($counter = 1)
 
-    <table border="1">
-        <thead>
-        <tr>
-            <th scope="col">Naam</th>
-            <th scope="col">Klas</th>
-            <th scope="col">Bezig met een opdracht</th>
-            <th scope="col">Aantal gemaakte opdrachten</th>
-            <th scope="col">Eindcijfer</th>
-            <th scope="col">Voortgang</th>
-        </tr>
-        </thead>
-        <tbody>
-            <tr>
-                <th scope="row">1</th>
-                <td>{{$student->name}} {{$student->insertion}} {{$student->last_name}}</td>
-                <td>{{$class}}</td>
-                <td>{{$doingAssignment}}</td>
-                <td>{{$totalAssignmentsDone}}</td>
-                <td>{{$endGrade}}</td>
-            </tr>
-        </tbody>
-    </table>
+    <div class="row">
+      <div class="col card">
+        <div class="card">
+          <div class="card-header">
+            Student van klas {{$class}}
+          </div>
+          <div class="card-body">
+            <table class="table table-striped">
+              <thead>
+                <tr>
+                  <th scope="col">Naam</th>
+                  <th scope="col">klas</th>
+                  <th scope="col">Bezig met een opdracht</th>
+                  <th scope="col">Aantal gemaakte opdrachten</th>
+                  <th scope="col">Eindcijfer</th>
+                  <th scope="col">Voortgang</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td class="center">{{$student->name}} {{$student->insertion}} {{$student->last_name}}</td>
+                  <td class="center">{{$class}}</td>
+                  <td class="center">{{$doingAssignment}}</td>
+                  <td class="center">{{$totalAssignmentsDone}}</td>
+                  <td class="center">{{$endGrade}}</td>
+                  <td class="center">Prgress</td>
+                </tr>
+              </tbody>
+            </table>
 
-    <table border="1">
-        <thead>
-        <tr>
-            <th scope="col">Opdracht nummer</th>
-            <th scope="col">Opdracht naam</th>
-            <th scope="col">Cijfer</th>
-        </tr>
-        </thead>
-        <tbody>
-        @foreach ($groupes as $groupe)
-            @php ($assignmentNumber = $groupe->assignment)
-            @php($assignment = App\Assignment::where('number', '=', $assignmentNumber)->first())
-            <tr>
-                <td>{{$assignmentNumber}}</td>
-                <td>{{$assignment->title}}</td>
-                <td>{{$groupe->grade}}</td>
-            </tr>
-        @endforeach
-        </tbody>
-    </table>
-
+            <table class="table table-striped">
+              <thead>
+                <tr>
+                  <th scope="col">Opdracht nummer</th>
+                  <th scope="col">Opdracht naam</th>
+                  <th scope="col">Cijfer</th>
+                </tr>
+              </thead>
+              <tbody>
+                  @foreach ($groupes as $groupe)
+                      @php ($assignmentNumber = $groupe->assignment)
+                      @php($assignment = App\Assignment::where('number', '=', $assignmentNumber)->first())
+                      <tr>
+                          <td class="center">{{$assignmentNumber}}</td>
+                          <td class="center">{{$assignment->title}}</td>
+                          <td class="center">{{$groupe->grade}}</td>
+                      </tr>
+                  @endforeach
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
 
 @endsection
