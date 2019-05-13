87c96d843258bcfa17710175b525aeea3083a931
Bas Mensinga <81314@b5-450-06.ict.lab.locals>
2019-05-13 14:10:56 +0200

    createCard is now more beautifull

--- resources/views/createCard.blade.php
+++ resources/views/createCard.blade.php
@@ -19,8 +19,8 @@
     <div class="col item">
       <div class="form-group col-md">
         <div class="createCard" onclick="select(1)">
-          <img src="{{ asset('images/cardCreate/scoreClasses.png') }}" alt="">
-          <p>Gemiddelde cijfer alle klassen</p>
+          <img src="{{ asset('images/cardCreate/scoreAllClasses.jpg') }}" alt="">
+          <p>Gemiddelde cijfer van alle klassen</p>
           <div class="allCards" id="1"></div>
         </div>
       </div>
@@ -29,8 +29,8 @@
     <div class="col item">
       <div class="form-group col-md">
         <div class="createCard" onclick="select(2)">
-          <img src="{{ asset('images/cardCreate/scoreClasses.png') }}" alt="">
-          <p>Gemiddelde cijfer een klas</p>
+          <img src="{{ asset('images/cardCreate/scoreClasses.jpg') }}" alt="">
+          <p>Gemiddelde cijfer van een klas</p>
           <div class="allCards" id="2"></div>
         </div>
       </div>
@@ -42,7 +42,7 @@
     <div class="col item">
       <div class="form-group col-md">
         <div class="createCard" onclick="select(3)">
-          <img src="{{ asset('images/cardCreate/scoreClasses.png') }}" alt="">
+          <img src="{{ asset('images/cardCreate/assignmentsAllClasses.jpg') }}" alt="">
           <p>Aantal gemaakte opdrachten van alle klassen</p>
           <div class="allCards" id="3"></div>
         </div>
@@ -52,16 +52,24 @@
     <div class="col item">
       <div class="form-group col-md">
         <div class="createCard" onclick="select(4)">
-          <img src="{{ asset('images/cardCreate/scoreClasses.png') }}" alt="">
-          <p>Aantal gemaakte opdrachten van een bepaalde klas</p>
+          <img src="{{ asset('images/cardCreate/assignmentsClasses.jpg') }}" alt="">
+          <p>Aantal gemaakte opdrachten van een klas</p>
           <div class="allCards" id="4"></div>
         </div>
       </div>
     </div>
 
   </div>
-    <input type="text" name="class">
-  <button type="submit" class="btn btn-primary" id="submitBtn"><p>Voeg kaart toe</p></button>
+
+  <div class="row input">
+    <div class="card">
+      <div class="card-body">
+        <input type="text" name="class" class="form-control" placeholder="Klasnaam indien nodig...">
+        <button type="submit" class="btn btn-primary" id="submitBtn"><p>Voeg kaart toe</p></button>
+      </div>
+    </div>
+  </div>
+
     {!! Form::close() !!}
 </div>
 @endsection
